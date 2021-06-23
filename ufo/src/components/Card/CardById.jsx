a9ce078a4d7701ef42599660fe8b4b6a656ed1ed
Thomas <thomas@avecnous.eu>
2021-06-23 16:22:29 +0200

    do an onClick function to charm Aliens

--- ufo/src/components/Card/CardById.jsx
+++ ufo/src/components/Card/CardById.jsx
@@ -4,12 +4,14 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { IoMdPlanet } from 'react-icons/io';
 import { WiAlien } from 'react-icons/wi';
+import Charm from './Charm';
 import './Card.css';
 
 export default function CardById() {
   const { id } = useParams();
   const [data, setData] = useState({});
   const [caractere, csetCaractere] = useState([]);
+  const [charm, setCharm] = useState(true);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -28,26 +30,46 @@ export default function CardById() {
 
   if (loading) return <div>Loading...</div>;
 
+  const charmAlien = () => {
+    setCharm(!charm);
+    setCharm(charm);
+    console.log(charm);
+  };
+
+  const toggleCharm = () => {
+    setTimeout(() => {
+      setCharm(!charm);
+    }, 200);
+  };
+
   const btnGender = data.gender;
 
   const genderAliens = () => {
     if (btnGender === 'F') {
       return (
-        <button type='button' className='charmeFemale'>
+        <button
+          type='button'
+          className='charmeFemale'
+          onClick={() => toggleCharm()}
+        >
           Charmez
         </button>
       );
     }
     if (btnGender === 'M') {
       return (
-        <button type='button' className='charmeMale'>
+        <button
+          type='button'
+          className='charmeMale'
+          onClick={() => toggleCharm()}
+        >
           Charmez
         </button>
       );
     }
     if (btnGender === 'Autre') {
       return (
-        <button type='button' className='charme'>
+        <button type='button' className='charme' onClick={() => toggleCharm()}>
           Charmez
         </button>
       );
@@ -65,15 +87,24 @@ export default function CardById() {
     species,
   } = data;
 
+  const message = `Votre charme a bien était envoyé à ${name} !`;
+
   return (
     <>
       <div className='ufoContainer'>
+        <div className={!charm ? 'displayCharm' : 'hideCharm'}>
+          Votre charme a bien était envoyé à{' '}
+          <span className='ufoNameCharm'>{name}</span>
+        </div>
         <div className='ufoImg'>
           <img src={image} alt='' />
         </div>
         <div className='ufoDescription'>
           <div className='ufoName'>
             <h2>{name}</h2>
+          </div>
+          <div className={charm ? 'heartCharm' : 'heartByDefault'}>
+            {charmAlien}
           </div>
           <div className='ufoAge'>
             <div className='ufoDesc'>
@@ -89,8 +120,8 @@ export default function CardById() {
           <div className='ufoPersonality'>
             <div className='alienType'>personnalité(e): </div>
             <div className='allPersonality'>
-              {caractere.map((data, id) => (
-                <div>{data}</div>
+              {caractere.map((data, i) => (
+                <div key={i}>{data}</div>
               ))}
             </div>
           </div>
